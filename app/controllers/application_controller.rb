b44e960c968a1edcc871c3b2349f29766c5eae06
tess323 <tess323@gmail.com>
2015-11-18 20:04:56 -0800

    first pass at auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   #auth stuff
+    def is_authenticated?
+    unless current_user
+      # flash[:danger] = "Credentials Invalid!!"
+      redirect_to login_path
+    end
+  end
+
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,21 @@
 class User < ActiveRecord::Base
+
+	# checks for email and checks that it is unique
+	validates :email,
+  		presence: true,
+  		uniqueness: {case_sensitive: false}
+
+
 	has_many :game
+
+	# checks for a password
+  	validates_presence_of :password, on: :create
+
+  	# calls bcrypt
+  	has_secure_password
+  	
+  	# authenticates email by password
+  def self.authenticate email, password
+      User.find_by_email(email).try(:authenticate, password)
+  end
 end
